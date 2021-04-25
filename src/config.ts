7da07d55cf196a8767828be648e00a6993faf22b
Jkuzz <honza.ku@gmail.com>
2021-04-25 15:51:44 +0200

    Implemented support for inherited enemy classes

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,22 @@
 export class EnemyConfig {
     static Basic: { [key:string]: any } = {
-        ['hp']: 20,
-        ['speed']: 1/20000,
-        ['money']: 1,
-        ['damage']: 1,
-        ['armor']: 0,
-        ['danger']: 10,
-        ['spriteName']: 'enemy1',
+        'hp': 20,
+        'speed': 1/20000,
+        'money': 1,
+        'damage': 1,
+        'armor': 0,
+        'danger': 10,
+        'spriteName': 'enemy1',
     }
 
     static Fat: { [key:string]: any } = {
-        ['hp']: 50,
-        ['speed']: 1/30000,
-        ['money']: 2,
-        ['damage']: 2,
-        ['armor']: 0,
-        ['danger']: 30,
-        ['spriteName']: 'enemy1',
+        'hp': 50,
+        'speed': 1/40000,
+        'money': 2,
+        'damage': 1,
+        'armor': 0,
+        'danger': 30,
+        'spriteName': 'enemy1',
     }
 
 
@@ -24,13 +24,13 @@ export class EnemyConfig {
 
 export class TowerConfig {
     static Basic: { [key:string]: any } = {
-        ['damage']: 10,
-        ['firerate']: 1,
-        ['range']: 150,
-        ['price']: 5,
-        ['spriteBase']: 'towerbase',
-        ['spriteMid']: 'towermid',
-        ['spriteTop']: 'towertop0',
+        'damage': 10,
+        'firerate': 1,
+        'range': 150,
+        'price': 5,
+        'spriteBase': 'towerbase',
+        'spriteMid': 'towermid',
+        'spriteTop': 'towertop0',
     }
 
 
