9f19d5bef6c582cfcfe5790be73ca067a93a186c
Petr Houška <houskape@gmail.com>
2021-04-25 20:02:52 +0200

    Sensible speedup.

--- src/playerInfo.ts
+++ src/playerInfo.ts
@@ -1,6 +1,6 @@
 export class PlayerInfo {
     static money: number = 10;
     static hp: number = 10;
-    static timeScale: number = 10;
+    static timeScale: number = 4;
     static RNG = new Phaser.Math.RandomDataGenerator(["42"])
 }
