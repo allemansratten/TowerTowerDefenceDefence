da9cd7edf7b0a82d20e9abd73992a7fc65224c4d
Petr Houška <houskape@gmail.com>
2021-05-03 21:01:38 +0200

    Add support for stable seed.

--- src/playerInfo.ts
+++ src/playerInfo.ts
@@ -5,10 +5,22 @@ export class PlayerInfo {
     static hp: number = 10;
     static timeScale: number = 1;
     static isPaused: boolean = false;
-    // static RNG = new Phaser.Math.RandomDataGenerator(["42"]);
+    
     static RNG = new Phaser.Math.RandomDataGenerator();
+    static LevelRNG = new Phaser.Math.RandomDataGenerator();
 
     static regenProgress: number = 0;
+
+    public static init()
+    {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const levelSeed = urlParams.get('levelSeed')
+
+        if (levelSeed) {
+            PlayerInfo.LevelRNG = new Phaser.Math.RandomDataGenerator([levelSeed]);
+        }
+    }
 
     public static waveHealthRegen() {
         PlayerInfo.regenProgress += PLAYER_HEALTH_REGEN;
